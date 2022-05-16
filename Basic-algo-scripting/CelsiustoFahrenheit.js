9170d6cb708304e132c194990172b4bc30982209
saideep2311 <saideep2311@gmail.com>
2022-05-16 17:28:29 +0530

    CtoF

---
+++ Basic-algo-scripting/CelsiustoFahrenheit.js
@@ -0,0 +1,7 @@
+// The formula to convert from Celsius to Fahrenheit is the temperature in Celsius times 9/5, plus 32.
+function convertToF(celsius) {
+  let fahrenheit = (celsius * 9) / 5 + 32;
+  return fahrenheit;
+}
+
+console.log(convertToF(30));
