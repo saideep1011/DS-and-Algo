e72664759744867bc49363b525144927d94949a9
saideep2311 <saideep2311@gmail.com>
2022-05-20 15:54:57 +0530

    countEqualand Div pairs

---
+++ leetcode/Arrays/countEqualAndDivPairs.js
@@ -0,0 +1,28 @@
+// 2176. Count Equal and Divisible Pairs in an Array
+
+// Given a 0-indexed integer array nums of length n and an integer k, return the number of pairs (i, j) where 0 <= i < j < n,
+// such that nums[i] == nums[j] and (i * j) is divisible by k.
+
+// Example 1:
+
+// Input: nums = [3,1,2,2,2,1,3], k = 2
+// Output: 4
+// Explanation:
+// There are 4 pairs that meet all the requirements:
+// - nums[0] == nums[6], and 0 * 6 == 0, which is divisible by 2.
+// - nums[2] == nums[3], and 2 * 3 == 6, which is divisible by 2.
+// - nums[2] == nums[4], and 2 * 4 == 8, which is divisible by 2.
+
+var countPairs = function (nums, k) {
+  let count = 0;
+  for (let i = 0; i < nums.length; i++) {
+    for (let j = i + 1; j < nums.length; j++) {
+      if (nums[i] == nums[j] && (i * j) % k == 0) {
+        count++;
+      }
+    }
+  }
+  return count;
+};
+
+console.log(countPairs([3, 1, 2, 2, 2, 1, 3], 2));
