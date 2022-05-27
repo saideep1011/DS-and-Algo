3124d7bfdc483de516f7374aec14521ba52009d0
saideep2311 <saideep2311@gmail.com>
2022-05-27 16:56:23 +0530

    sliding window on sub arrays init

--- leetcode/Arrays/EASY_LEVEL-1/MaxSumSubArrayofsizeK.js
+++ leetcode/Arrays/EASY_LEVEL-1/MaxSumSubArrayofsizeK.js
@@ -22,21 +22,44 @@
 // let window sum= current sum of window
 // max sum is highest window sum found
 //
-const max_sub_array_of_size_k = function (arr, k) {
-  let windowSum = 0;
+// const max_sub_array_of_size_k = function (arr, k) {
+//   let windowSum = 0;
+//   let maxSum = 0;
+//   let i;
+//   for (i = 0; i < k; i++) {
+//     windowSum += arr[i];
+//     maxSum = windowSum;
+//     // here we got the sum of first three elements
+//   }
+//   // now lets consider case where i=k or greater than k
+//   for (let i = k; i < arr.length; i++) {
+//     windowSum += arr[i] - arr[i - k];
+//     // here we are adding next element and deleting previous element
+//     maxSum = Math.max(windowSum, maxSum);
+//   }
+//   return maxSum;
+// };
+// console.log(max_sub_array_of_size_k([2, 1, 5, 1, 3, 2], 3));
+
+// now lets write same code with different approach and better one for all cases of given subarray length k
+
+const maxSubArrays = function (nums, k) {
+  let i = 0;
+  let j = 0;
+  let currentSum = 0;
   let maxSum = 0;
-  let i;
-  for (i = 0; i < k; i++) {
-    windowSum += arr[i];
-    maxSum = windowSum;
-    // here we got the sum of first three elements
-  }
-  // now lets consider case where i=k or greater than k
-  for (let i = k; i < arr.length; i++) {
-    windowSum += arr[i] - arr[i - k];
-    // here we are adding next element and deleting previous element+
-    maxSum = Math.max(windowSum, maxSum);
+  while (j < nums.length) {
+    currentSum = currentSum + nums[j];
+    if (j - i + 1 < k) {
+      j++;
+    } else if (j - i + 1 == k) {
+      maxSum = Math.max(currentSum, maxSum);
+      currentSum -= nums[i];
+
+      i++;
+      j++;
+    }
   }
   return maxSum;
 };
-console.log(max_sub_array_of_size_k([2, 1, 5, 1, 3, 2], 3));
+console.log(maxSubArrays([2, 1, 5, 1, 3, 2], 3));
